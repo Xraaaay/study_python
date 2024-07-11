29e3c578b32b1b2409d4d4d1acef31862aaddbf5
Xraaaay <xiaorw1227@gmail.com>
2024-07-11 20:31:21 +0800

    feat: add range.py

---
+++ src/00_basic/01_built_in_function/range.py
@@ -0,0 +1,28 @@
+"""
+@Description: python doc: https://docs.python.org/zh-cn/3/library/functions.html
+@Author: Xraaaay
+@Time: 2024/7/11
+"""
+
+
+def _range_01() -> None:
+    """
+    range() : Return an object that produces a sequence of integers from start (inclusive)
+    to stop (exclusive) by step.
+    """
+    for i in range(5):
+        print(i)
+
+
+def _range_02() -> None:
+    for i in range(1, 9, 2):
+        print(i)
+
+
+def main() -> None:
+    _range_01()
+    _range_02()
+
+
+if __name__ == '__main__':
+    main()
